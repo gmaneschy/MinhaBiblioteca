3ff51636e1c47ba89200a75ebc868ea85da73046
gmaneschy <145074809+gmaneschy@users.noreply.github.com>
2025-05-24 14:46:00 -0300

    .

--- biblioteca/lib/forms.py
+++ biblioteca/lib/forms.py
@@ -15,18 +15,6 @@ class LivroForm(forms.ModelForm):
             'ano': 'Ano de Publicação',
             'preco': 'Preço',
             'status': 'Status',
-            'anotacoes': 'Anotações',
+            'anotacoes': 'Anotações'
         }
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['titulo'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Título'})
-            self.fields['autor'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Autor'})
-            self.fields['editora'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Editora'})
-            self.fields['tradutor'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Tradutor'})
-            self.fields['genero'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Gênero'})
-            self.fields['npaginas'].widget.attrs.update({'class': 'form-control', 'placeholder': 'N° de Páginas'})
-            self.fields['ano'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Ano de Publicação'})
-            self.fields['preco'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Preço'})
-            self.fields['status'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Status'})
-            self.fields['anotacoes'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Anotações'})
