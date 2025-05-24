0b4742fcc46a38d41b4bf14ca5f2f497de936865
gmaneschy <145074809+gmaneschy@users.noreply.github.com>
2025-05-24 10:09:47 -0300

    Pesquisar corrigido

--- biblioteca/lib/forms.py
+++ biblioteca/lib/forms.py
@@ -4,4 +4,5 @@ from .models import Livro  # ajusta o nome conforme seu model
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
-        fields = ['titulo', 'autor', 'editora', 'tradutor', 'genero', 'npaginas', 'ano', 'preco', 'status', 'anotacoes']  # coloque os campos corretos do seu model
+        fields = ['titulo', 'autor', 'editora']  # coloque os campos corretos do seu model
+        # 'tradutor', 'genero', 'npaginas', 'ano', 'preco', 'status', 'anotacoes'
